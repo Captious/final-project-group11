404b9ea6708934c70f9c6a8b528b6019a05bba80
Sergey <sevladem@gmail.com>
2017-05-06 16:48:17 +0300

    [08] - ready for review

--- FinalProject/src/main/java/ua/goit/java/hotelbooking/services/impl/UserManageServiceImpl.java
+++ FinalProject/src/main/java/ua/goit/java/hotelbooking/services/impl/UserManageServiceImpl.java
@@ -1,18 +1,22 @@
 package ua.goit.java.hotelbooking.services.impl;
 
+import ua.goit.java.hotelbooking.dao.UserDao;
+import ua.goit.java.hotelbooking.dao.impl.UserDaoImpl;
 import ua.goit.java.hotelbooking.model.User;
 import ua.goit.java.hotelbooking.services.UserManageService;
 
 public class UserManageServiceImpl implements UserManageService{
 
+    private UserDao userDao = UserDaoImpl.getInstance();
+
     @Override
     public void add(User element) {
-
+        userDao.persist(element);
     }
 
     @Override
     public User edit(User element) {
-        return null;
+        return userDao.persist(element);
     }
 
     @Override
