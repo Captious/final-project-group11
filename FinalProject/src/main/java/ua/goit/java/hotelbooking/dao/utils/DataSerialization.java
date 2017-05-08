4a075f3833d2fb8a5234b26e39eb130703fb9e8d
DemetriosM <dmdonetsk@outlook.com>
2017-05-08 09:21:41 +0300

    In class DataSerialization removed the unnecessary blocks 'catch'.

--- FinalProject/src/main/java/ua/goit/java/hotelbooking/dao/utils/DataSerialization.java
+++ FinalProject/src/main/java/ua/goit/java/hotelbooking/dao/utils/DataSerialization.java
@@ -8,9 +8,6 @@ public class DataSerialization {
     public static Object deserializeData(String filePath){
         try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(filePath))){
             return in.readObject();
-            //TODO: Please, use multiple catching for all exceptions.
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -20,10 +17,6 @@ public class DataSerialization {
     public static void serializeData(String filePath, Object obj){
         try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))){
             out.writeObject(obj);
-            //TODO: Please, use multiple catching for all exceptions.
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            //TODO: Please, use multiple catching.
         } catch (IOException e) {
             e.printStackTrace();
         }
